50fa09f00824afb6deac1c04bd03101f558c8da5
ry2404 <74439020+yrz1997@users.noreply.github.com>
2021-02-15 20:14:24 -0500

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -33,6 +33,10 @@ ui = shiny::htmlTemplate(
   # Index Page
   "www/custom_group9.html",
   
+  total_cases_text = textOutput("total_cases",inline = TRUE),
+  total_death_text = textOutput("total_death",inline = TRUE),
+  total_recovered_text = textOutput("total_recovered",inline = TRUE),
+  
   # Vaccine timeline
   rec_selector= checkboxInput("rec", "Show Recovered", value = TRUE, width = NULL),
   vac_selector = checkboxInput("vac", "Show Vaccinated", value = TRUE, width = NULL),
@@ -56,15 +60,21 @@ ui = shiny::htmlTemplate(
                 max = as.Date(tail(date_choices,1)),
                 value = as.Date('2020-04-01','%Y-%m-%d'),
                 timeFormat = "%Y-%m-%d",
-                animate = TRUE, step = 5),
+                step = 5,
+                animate = animationOptions(
+                  playButton = HTML("<img src='images/icons/play-button.png' height='20' width='20'>"), 
+                  pauseButton = HTML("<img src='images/icons/pause-button.png' height='20' width='20'>"))
+  ),
   
   country_selector=selectInput("select_country", label = h3("Select Country"), 
                                choices = ISO_Name_Vec, 
                                selected = "ISR"),
   
   map_selector=selectInput('choices','Which data to visualize:',
-                          choices = c('Cases','Death'),
-                          selected = c('Cases')),
+                          choices = c('cases','death','vaccination'),
+                          selected = 'cases'),
+  
+  leaflet_map = leafletOutput(outputId = "map",width = "100%",height = "600"),
   
   mortality_years=sliderInput("year_mortality", "Mortality Years", 2015, 2021, value = c(2018, 2021))
   
